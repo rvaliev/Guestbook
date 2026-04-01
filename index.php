a4aaa583c2f650dd8720b0ba8ac0392b5aa9f7da
Ruslan <rvaliev@users.noreply.github.com>
2026-04-02 00:58:19 +0200

    Update index.php file with new guestbook and contact form functionality

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once("classes/guestbook.class.php");
 </head>
 <body>
 <div class="wrapper">
-
+ 
     <h1>Berichten!!</h1>
 
     <?php
@@ -58,7 +58,6 @@ require_once("classes/guestbook.class.php");
         $_SESSION['message'] = $message;
 
 
-
         if ((!empty($author)) && (!empty($message)))
         {
             $messageLength = strlen($message);
@@ -86,6 +85,80 @@ require_once("classes/guestbook.class.php");
     }
     ?>
 
+    <hr/>
+    <h2>Contact Us</h2>
+    
+    <?php
+    if (isset($_SESSION['contactMessage'])) {
+        echo $_SESSION['contactMessage'];
+        unset($_SESSION['contactMessage']);
+    }
+    ?>
+
+    <form action="" method="post">
+        <label for="contact_name">Name: </label>
+        <br>
+        <input type="text" name="contact_name" id="contact_name" placeholder="Your Name" value="<?= (isset($_SESSION['contact_name'])) ? $_SESSION['contact_name'] : "";?>"/>
+        <br>
+        <label for="contact_email">Email: </label>
+        <br>
+        <input type="email" name="contact_email" id="contact_email" placeholder="Your Email" value="<?= (isset($_SESSION['contact_email'])) ? $_SESSION['contact_email'] : "";?>"/>
+        <br>
+        <label for="contact_subject">Subject: </label>
+        <br>
+        <input type="text" name="contact_subject" id="contact_subject" placeholder="Subject" value="<?= (isset($_SESSION['contact_subject'])) ? $_SESSION['contact_subject'] : "";?>"/>
+        <br>
+        <label for="contact_message">Message: </label>
+        <br>
+        <textarea cols="40" rows="3" name="contact_message" id="contact_message" placeholder="Your Message"><?= (isset($_SESSION['contact_message'])) ? $_SESSION['contact_message'] : "";?></textarea>
+        <br>
+        <input type="submit" name="contactBtn" value="Send Contact"/>
+    </form>
+
+    <?php
+    if (isset($_POST['contactBtn']))
+    {
+        $contact_name = $_POST['contact_name'];
+        $contact_email = $_POST['contact_email'];
+        $contact_subject = $_POST['contact_subject'];
+        $contact_message = $_POST['contact_message'];
+        
+        $_SESSION['contact_name'] = $contact_name;
+        $_SESSION['contact_email'] = $contact_email;
+        $_SESSION['contact_subject'] = $contact_subject;
+        $_SESSION['contact_message'] = $contact_message;
+
+        if ((!empty($contact_name)) && (!empty($contact_email)) && (!empty($contact_subject)) && (!empty($contact_message)))
+        {
+            $messageLength = strlen($contact_message);
+            if ($messageLength > 500) {
+                $_SESSION['contactMessage'] = "<p class=\"redMessage\">Message moet max 500 karakters zijn</p>";
+            }
+            else{
+                $to = "admin@example.com";
+                $headers = "From: " . $contact_name . " <" . $contact_email . ">\r\n";
+                $headers .= "Reply-To: " . $contact_email . "\r\n";
+                
+                if (mail($to, $contact_subject, $contact_message, $headers)) {
+                    $_SESSION['contactMessage'] = "<p class=\"greenMessage\">Contact form submitted successfully</p>";
+                    $_SESSION['contact_name'] = null;
+                    $_SESSION['contact_email'] = null;
+                    $_SESSION['contact_subject'] = null;
+                    $_SESSION['contact_message'] = null;
+                }
+                else{
+                    $_SESSION['contactMessage'] = "<p class=\"redMessage\">Something went wrong sending your message</p>";
+                }
+            }
+        }
+        else
+        {
+            $_SESSION['contactMessage'] = "<p class=\"redMessage\">All fields are required</p>";
+        }
+
+        header("Refresh:0");
+    }
+    ?>
 
 </div>
 </body>
